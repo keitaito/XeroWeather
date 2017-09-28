da3cf02fd7157762282c0d16d92826deaea91850
Keita Ito <keitaitok@gmail.com>
2017-09-27 21:49:24 -0700

    Add XeroWeatherCore as Linked Framework

--- XeroWeather/ViewController.swift
+++ XeroWeather/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import XeroWeatherCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let weather = Weather(cityName: "San Fransokyo")
+        title = weather.cityName
     }
 
     override func didReceiveMemoryWarning() {
