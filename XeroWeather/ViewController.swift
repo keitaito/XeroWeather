aa84a6bdad3ff4d2039e46394aa52537eec5b2f4
Keita Ito <keitaitok@gmail.com>
2017-09-27 21:11:11 -0700

    Initial Commit

---
+++ XeroWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XeroWeather
+//
+//  Created by Keita Ito on 9/27/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
