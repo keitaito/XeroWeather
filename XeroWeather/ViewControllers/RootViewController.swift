717f1ae233030d0d7351318974add6372cfe1274
Keita Ito <keitaitok@gmail.com>
2017-10-03 17:50:34 -0700

    Add RootViewController.swift

---
+++ XeroWeather/ViewControllers/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RootViewController.swift
+//  XeroWeather
+//
+//  Created by Keita Ito on 10/3/17.
+//  Copyright © 2017 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
